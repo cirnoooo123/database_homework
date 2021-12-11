2ad9a5986f083812500ed36de9cf69f18a42d318
苏俊喆 <19373074@buaa.edu.cn>
2021-12-11 16:19:09 +0800

    新增验证码 ——苏俊喆

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', include('login.urls')),
+    path('captcha/', include('captcha.urls')),
 ]
